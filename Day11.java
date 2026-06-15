78444a21b86adc20fbf7805d42903a8adb2ad82c
syedrazahussain <srazahussain1514@gmail.com>
2026-06-15 13:24:54 +0530

    Day 11 important logic building problem practised

--- Day11.java
+++ Day11.java
@@ -6,20 +6,46 @@ class Day11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // question 1 : Check Prime number or not
-        System.out.println("Enter any Number : ");
-        int n = sc.nextInt();
-        int count = 0;
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) {
-                count++;
-            }
+        // System.out.println("Enter any Number : ");
+        // int n = sc.nextInt();
+        // int count = 0;
+        // for (int i = 1; i <= n; i++) {
+        //     if (n % i == 0) {
+        //         count++;
+        //     }
+        // }
+        // if(count == 2){
+        //     System.out.println("Its Prime Number");
+        // }
+        // else {
+        //     System.out.println("Not a Prime number");
+        // }
+
+        //question 2 : print Factorial check 
+        // System.out.println("Enter any Number : ");
+        // int num = sc.nextInt();
+        // int fact = 1;
+        // for(int i = 1; i<=num; i++){
+        //     fact*= i;
+        // }
+        // System.out.println("The Factorial Number : " +fact);
+
+        //question 3 : Print Fibonacci series
+        System.out.println("Enter number Range for series : ");
+        int num1 = sc.nextInt();
+        int firstSum = 0;
+        int secondSum = 1;
+        int nextSum;
+        for(int i = 0; i<=num1; i++){
+            System.out.print(firstSum + " ");
+            nextSum = firstSum + secondSum;
+            firstSum = secondSum;
+            secondSum = nextSum;
+
+
         }
-        if(count == 2){
-            System.out.println("Its Prime Number");
-        }
-        else {
-            System.out.println("Not a Prime number");
-        }
+
+
 
     }
 }
