94eabf07ca49212a753bed883714e36bedb66ead
syedrazahussain <srazahussain1514@gmail.com>
2026-06-15 14:14:16 +0530

    Day 11 important logic building problem practised

--- Day11.java
+++ Day11.java
@@ -10,42 +10,51 @@ class Day11 {
         // int n = sc.nextInt();
         // int count = 0;
         // for (int i = 1; i <= n; i++) {
-        //     if (n % i == 0) {
-        //         count++;
-        //     }
+        // if (n % i == 0) {
+        // count++;
+        // }
         // }
         // if(count == 2){
-        //     System.out.println("Its Prime Number");
+        // System.out.println("Its Prime Number");
         // }
         // else {
-        //     System.out.println("Not a Prime number");
+        // System.out.println("Not a Prime number");
         // }
 
-        //question 2 : print Factorial check 
+        // question 2 : print Factorial check
         // System.out.println("Enter any Number : ");
         // int num = sc.nextInt();
         // int fact = 1;
         // for(int i = 1; i<=num; i++){
-        //     fact*= i;
+        // fact*= i;
         // }
         // System.out.println("The Factorial Number : " +fact);
 
-        //question 3 : Print Fibonacci series
-        System.out.println("Enter number Range for series : ");
-        int num1 = sc.nextInt();
-        int firstSum = 0;
-        int secondSum = 1;
-        int nextSum;
-        for(int i = 0; i<=num1; i++){
-            System.out.print(firstSum + " ");
-            nextSum = firstSum + secondSum;
-            firstSum = secondSum;
-            secondSum = nextSum;
+        // question 3 : Print Fibonacci series
+        // System.out.println("Enter number Range for series : ");
+        // int num1 = sc.nextInt();
+        // int firstSum = 0;
+        // int secondSum = 1;
+        // int nextSum;
+        // for(int i = 0; i<=num1; i++){
+        // System.out.print(firstSum + " ");
+        // nextSum = firstSum + secondSum;
+        // firstSum = secondSum;
+        // secondSum = nextSum;
 
+        // }
 
+        // question 4 : Reverse number
+        System.out.println("Enter any number : ");
+        int num2 = sc.nextInt();
+        int revnum = 0;
+        while(num2 > 0) {
+            int rem = num2 % 10;
+            revnum = revnum * 10 + rem;
+            num2 = num2 / 10;
         }
-
-
+        System.out.println(revnum);
+        
 
     }
 }
