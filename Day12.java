bea645a3251c4ca5b3c86e803086b1ea69c40f7e
syedrazahussain <srazahussain1514@gmail.com>
2026-06-23 13:48:09 +0530

    Day 12 imporatnt  problem practised

--- Day12.java
+++ Day12.java
@@ -6,21 +6,21 @@ import java.util.Scanner;
 class Day12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // question 1 : The sum of the digits
+        question 1 : The sum of the digits
 
-        // System.out.println("Enter any number : ");
-        // int no = sc.nextInt();
+        System.out.println("Enter any number : ");
+        int no = sc.nextInt();
 
-        // int temp = no;
-        // int sum = 0;
+        int temp = no;
+        int sum = 0;
 
-        // while (temp > 0) {
-        //     temp = no % 10; // 123 ==> 3
-        //     sum += temp; // sum =0+3 =3
-        //     no = no / 10; // 123/10 ==>12
+        while (temp > 0) {
+            temp = no % 10; // 123 ==> 3
+            sum += temp; // sum =0+3 =3
+            no = no / 10; // 123/10 ==>12
 
-        // }
-        // System.out.println("The sum of the digits is : " + sum);
+        }
+        System.out.println("The sum of the digits is : " + sum);
 
         // question 2 : The GCD or lcm of the number
         System.out.print("Enter value of A : ");
