9904c96f18278a3249c6cf5e035cbc0baa17daf9
syedrazahussain <srazahussain1514@gmail.com>
2026-07-08 13:54:51 +0530

    Day 15 nested if  problem  practised

--- Day15.java
+++ Day15.java
@@ -125,41 +125,147 @@ class Day15 {
         }
     }
 
-    
+    static void printDay(){
+        System.out.println("Enter Number (1-7)");
+        int number = sc.nextInt();
+        switch (number) {
+            case 1:
+                System.out.println("Monday");
+                
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                
+                break;
+            case 4:
+                System.out.println("Thursday");
+                
+                break;
+            case 5:
+                System.out.println("Friday");
+                
+                break;
+            case 6:
+                System.out.println("Saturday");
+                
+                break;
+            case 7:
+                System.out.println("Sunday");
+                
+                break;
+        
+            default:
+                System.out.println("Invalid number");
+                break;
+        }
+    }
+    static void printNoofdays(){
+        System.out.println("Enter Month number (1-12)");
+        int number = sc.nextInt();
+        switch (number) {
+            case 1:
+                System.out.println("31");
+                
+                break;
+            case 2:
+                System.out.println("28");
+                
+                break;
+            case 3:
+                System.out.println("31");
+                
+                break;
+            case 4:
+                System.out.println("30");
+                
+                break;
+            case 5:
+                System.out.println("31");
+                
+                break;
+            case 6:
+                System.out.println("30");
+                
+                break;
+            case 7:
+                System.out.println("31");
+                
+                break;
+            case 8:
+                System.out.println("30");
+                
+                break;
+            case 9:
+                System.out.println("31");
+                
+                break;
+            case 10:
+                System.out.println("30");
+                
+                break;
+            case 11:
+                System.out.println("31");
+                
+                break;
+            case 12:
+                System.out.println("31");
+                
+                break;
+        
+            default:
+                System.out.println("Invalid number");
+                break;
+        }
+
+    }
+
+
     public static void main(String[] args) {
 
-        // System.out.println("Enter A Value : ");
-        // int a = sc.nextInt();
-        // System.out.println("Enter B Value : ");
-        // int b = sc.nextInt();
-        // System.out.println("Enter C Value : ");
-        // int c = sc.nextInt();
+        System.out.println("Enter A Value : ");
+        int a = sc.nextInt();
+        System.out.println("Enter B Value : ");
+        int b = sc.nextInt();
+        System.out.println("Enter C Value : ");
+        int c = sc.nextInt();
 
         // Q1. Take three sides and check if they form a valid triangle.
-        // checkValidTraingle(a,b,c);
+        checkValidTraingle(a,b,c);
 
         // Q2. if the sides form a valid triangle, determine whether it is equilateral,
         // isosceles, or scalene
-        // checkwhichTypeTraingle(a, b, c);
+        checkwhichTypeTraingle(a, b, c);
 
         // Q3. Take marks (0-100) and print the corresponding grade (A/B/C/D/F)
-        // printgrade();
+        printgrade();
 
         // Q4. Check if one of two given numbers is a multiple of the other
-        // check2multiplenumbers();
+        check2multiplenumbers();
 
         // Q5. Take the hour of the day (0-23) and print "Good morning","Good
         // afternoon","Good Evening","Good Night"
-        // greetingwithHour();
+        greetingwithHour();
 
         // Q6. Check Voting eligibilty for a given age(18+)
-        // checkVotingAge();
+        checkVotingAge();
 
         //Q7. Take two numbers and determine whether both are even, both are odd, or one is even and one is odd
-        // checkevenoroddWith2num();
+        checkevenoroddWith2num();
 
         //Q8. Take a alphabet character and check if it lies between ' a' and 'm' or 'n' and 'z'
         checkalpahbet();
+
+        //Q9. Take a day number (1-7) and print the corresponding day name
+        printDay();
+
+        //Q10. Take a month number (1-12) and print the number of days in that month (ignore)
+        //leap year
+        printNoofdays();
+
 
     }
 }
