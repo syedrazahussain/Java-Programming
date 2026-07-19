eb4769bb1cee9b9d4e9c3cc57d6847fab3797c26
syedrazahussain <srazahussain1514@gmail.com>
2026-07-19 12:44:06 +0530

    Day 21 phase-2 level -3 mathematical & logical pattern problem  practised

---
+++ Day21.java
@@ -0,0 +1,116 @@
+//=====Day 20 (Phase 2 looping & patterns (itreation & flow) )==================
+//Level 3 : Mathematical & Logical patterns
+
+import java.util.Scanner;
+
+class Day21 {
+    static Scanner sc = new Scanner(System.in);
+
+    static void printsquaresnumber() {
+        System.out.println("Enter Any number : ");
+        int number = sc.nextInt();
+        for (int i = 0; i <= number - 1; i++) {
+            System.out.print(i * i + " ");
+        }
+
+    }
+
+    static void printcubenumber() {
+        System.out.println("Enter Any number : ");
+        int number = sc.nextInt();
+        for (int i = 0; i <= number - 1; i++) {
+            System.out.print(i * i * i + " ");
+        }
+
+    }
+
+    static void printallnumberdivisibleby7() {
+        System.out.println("Enter A value : ");
+        int a = sc.nextInt();
+        System.out.println("Enter B Value : ");
+        int b = sc.nextInt();
+        for (int i = a; i <= b; i++) {
+            if (i % 7 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+    }
+
+    static void findHCFNumber() {
+        System.out.println("Enter A Value : ");
+        int a = sc.nextInt();
+        System.out.println("Enter B value : ");
+        int b = sc.nextInt();
+        int hcf = 0;
+        if (a > b) {
+            for (int i = 1; i <= b; i++) {
+                if (a % i == 0 && b % i == 0) {
+                    hcf = i;
+
+                }
+            }
+            System.out.print(hcf + " ");
+        }
+
+        else {
+            for (int i = 1; i <= a; i++) {
+                if (a % i == 0 && b % i == 0) {
+                    hcf = i;
+
+                }
+            }
+            System.out.print(hcf + " ");
+
+        }
+
+    }
+
+    static void printLcmNumber() {
+        System.out.println("Enter A Value : ");
+        int a = sc.nextInt();
+        System.out.println("Enter B value : ");
+        int b = sc.nextInt();
+        int lcm = 0;
+        if (a > b) {
+            int i = a;
+            while (true) {
+                if (i % a == 0 && i % b == 0) {
+                    lcm = i;
+                    break;
+                }
+                i++;
+            }
+            System.out.println("Lcm : " + lcm);
+        } else {
+            int i = b;
+            while (true) {
+                if (i % a == 0 && i % b == 0) {
+                    lcm = i;
+                    break;
+                }
+                i++;
+            }
+            System.out.println("Lcm : " + lcm);
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        // Q1. Print the squares of number from 1 to n
+        // printsquaresnumber();
+
+        // Q2. print cubes of number from 1 to n
+        // printcubenumber();
+
+        // Q3. print all numbers between a and b divisible by 7.
+        // printallnumberdivisibleby7();
+
+        // Q4. find HCF (GCD) of two numbers using loops.
+        // findHCFNumber();
+
+        // Q5. Find LCM of two numbers using loops.
+        // printLcmNumber();
+    }
+}
